d3200f7960d014e9297e01628ce9c7eb280ffe7e
giammy00 <puleogianm@gmail.com>
2022-12-13 18:35:32 +0100

    fix typo and adjusted input shape

--- plotter.py
+++ plotter.py
@@ -8,12 +8,18 @@ plt.rc('font', family='serif')
 
 def plot_eigenvector_components_convergence(time, time_evolution_array):
     """
-    Takes the time evolution of the point and plots the path for each component
+    Takes the time evolution of the point and plots the path for each component.
+
+    args:
+        time: array (or tensor) containing N timesteps
+        time_evolution_array: np.ndarray (or tf.Tensor) of shape (N, dim), dim=dimension of space.
     """
     fig = plt.figure(figsize=figsize)
     plt.xlabel('time [-]')
     plt.ylabel('Eigenvector Components')
 
+    #transpose to make it of shape (dim, N), for loop happens along axis 0.
+    time_evolution_array = tf.transpose(time_evolution_array)
     for component in time_evolution_array:
         plt.plot(time, component, lw=2, zorder=1)
         x_cordinate = time[-1]
@@ -46,6 +52,6 @@ def plot_estimated_eigenvetors(epochs, final_eigenvect_evolution):
     plt.ylabel('Eigenvector Components')
 
     for component in final_eigenvect_evolution:
-        plt.plot(epoch, component), zorder=1, lw=2)
+        plt.plot(epoch, component, zorder=1, lw=2)
 
     return fig
